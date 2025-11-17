c8dd75cece8c26e70387575ab18cd444aa78b039
Yinka Makanjuola <yinka.makanjuola@gmail.com>
2025-11-17 10:46:27 +0100

    PING: respond with pong for all incoming commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,8 +10,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write_all(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);

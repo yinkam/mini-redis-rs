699ccb9baa7aca029e12bdcc149f205dcc9da691
Yinka Makanjuola <yinka.makanjuola@gmail.com>
2025-11-17 14:20:44 +0100

    PING: spawn new threads for each connection request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::thread;
+
+
+fn handler(mut stream: &TcpStream) {
+    println!("Connection from {}", stream.peer_addr().unwrap());
+    let mut buffer = [0; 1024];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(bytes) => {
+                if bytes == 0 {
+                    break;
+                }
+                let received_data = String::from_utf8_lossy(&buffer[..bytes]);
+                match received_data.to_string().as_str() {
+                    "PING\n" => {
+                        stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                    _ => {
+                        stream.write_all(b"+PONG\r\n").unwrap();
+                    }
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,28 +41,9 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 1024];
-                loop {
-                    match _stream.read(&mut buffer) {
-                        Ok(bytes) => {
-                            if bytes == 0 {
-                                break;
-                            }
-                            let received_data = String::from_utf8_lossy(&buffer[..bytes]);
-                            match received_data.to_string().as_str() {
-                                "PING\n" => {
-                                    _stream.write_all(b"+PONG\r\n").unwrap();
-                                }
-                                _ => {
-                                    _stream.write_all(b"+PONG\r\n").unwrap();
-                                }
-                            }
-                        }
-                        Err(e) => {
-                            println!("error: {}", e);
-                        }
-                    }
-                }
+                thread::spawn( move || {
+                    handler(&_stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);

f56a6ae1932963aa33a504c64cc6421b317f1cc8
Yinka Makanjuola <yinka.makanjuola@gmail.com>
2025-12-08 14:33:31 +0100

    implement eventloop for non-blocking io. clean up unused packages

---
+++ src/event_loop.rs
@@ -0,0 +1,83 @@
+use crate::cache::Cache;
+use mio::net::{TcpListener, TcpStream};
+use mio::{Events, Interest, Poll, Token};
+use std::collections::HashMap;
+use std::io::Error;
+use std::io::ErrorKind::WouldBlock;
+use std::net::SocketAddr;
+use std::str::FromStr;
+
+pub struct EventLoop {
+    listener: TcpListener,
+    poll: Poll,
+    events: Events,
+    connections: HashMap<Token, TcpStream>,
+}
+
+impl EventLoop {
+    pub fn new(address: &str) -> Self {
+        let address = SocketAddr::from_str(address).unwrap();
+        let listener = TcpListener::bind(address).unwrap();
+        let poll = match Poll::new() {
+            Ok(poll) => poll,
+            Err(e) => panic!("Unable to create poll listener: {:?}", e),
+        };
+
+        let events = Events::with_capacity(1024);
+        let connections = HashMap::new();
+
+        Self {
+            listener,
+            poll,
+            events,
+            connections,
+        }
+    }
+
+    pub fn run<F>(&mut self, mut db: Cache, handler: F) -> Result<(), Error>
+    where
+        F: Fn(&TcpStream, &mut Cache),
+    {
+        const SERVER: Token = Token(0);
+        let mut next_token = 1;
+
+        self.poll
+            .registry()
+            .register(&mut self.listener, SERVER, Interest::READABLE)?;
+
+        loop {
+            self.poll.poll(&mut self.events, None)?;
+
+            for event in self.events.iter() {
+                match event.token() {
+                    SERVER => loop {
+                        match self.listener.accept() {
+                            Ok((mut socket, addr)) => {
+                                println!("new connection from {}", addr);
+                                let client = Token(next_token);
+                                next_token += 1;
+
+                                self.poll.registry().register(
+                                    &mut socket,
+                                    client,
+                                    Interest::READABLE | Interest::WRITABLE,
+                                )?;
+                                self.connections.insert(client, socket);
+                            }
+                            Err(ref err) if err.kind() == WouldBlock => break,
+                            Err(err) => return Err(err),
+                        }
+                    },
+                    client => {
+                        let socket = match self.connections.get_mut(&client) {
+                            Some(socket) => socket,
+                            None => continue,
+                        };
+
+                        handler(socket, &mut db);
+                    }
+                }
+            }
+        }
+    }
+}

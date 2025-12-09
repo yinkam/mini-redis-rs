60b7114f5540f02be083cbd4f434740866667e56
Yinka Makanjuola <yinka.makanjuola@gmail.com>
2025-12-09 11:01:52 +0100

    REPLICATION: execute info command to retrieve basic server info

--- src/handler.rs
+++ src/handler.rs
@@ -34,6 +34,7 @@ fn process_command(mut stream: &TcpStream, db: &mut Cache, command: &Value) {
                 "ECHO" => stream.write_all(&arr[1].to_resp()).unwrap(),
                 "SET" => execute_set(stream, arr, db),
                 "GET" => execute_get(stream, arr, db),
+                "INFO" => execute_info(stream, arr, db),
                 _ => stream.write_all(b"-ERR Unknown command\r\n").unwrap(),
             },
             _ => println!("Invalid command"),
@@ -94,4 +95,11 @@ fn execute_get(mut stream: &TcpStream, arr: &Vec<Value>, db: &mut Cache) {
 
     let value = &db.get(&key).unwrap_or(null_bulk_string);
     stream.write_all(value).unwrap()
+}
+
+
+fn execute_info(mut stream: &TcpStream, _arr: &Vec<Value>, _db: &mut Cache) {
+    let server_info = BulkString("role:master".to_string());
+
+    stream.write_all(&server_info.to_resp()).unwrap()
 }
